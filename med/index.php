c1c8d2a40b6ecf965fc434abe540c4039363d601
Mikaido100 <mikaido100@gmail.com>
2022-03-22 12:53:15 +0100

    2

--- med/index.php
+++ med/index.php
@@ -1,5 +1,24 @@
 <?php
 
-echo"hello world";
 
+$db = new mysqli("localhost", "root", "", "med");
+
+$q =$db->prepare("SELECT * FROM staff" );
+
+
+if($q->execute()) {
+
+    $result =$q->get_result();
+    while($row =$result->fetch_assoc()) {
+        $firstname = $row['first name'];
+        $lastname = $row["last name"];
+        echo "lekarz $firstname $lastname<br>";
+    }
+} else {
+    echo "błąd podczas wyszukiwania w bazie danych";
+}
+
+
+
+ 
 ?>
