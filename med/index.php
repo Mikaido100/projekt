20e2ef2ce4888a155ab25edec62de213d1f135d0
Mikaido100 <mikaido100@gmail.com>
2022-03-22 13:48:00 +0100

    3

--- med/index.php
+++ med/index.php
@@ -10,9 +10,22 @@ if($q->execute()) {
 
     $result =$q->get_result();
     while($row =$result->fetch_assoc()) {
+        $staff_id = $row['id'];
         $firstname = $row['first name'];
         $lastname = $row["last name"];
         echo "lekarz $firstname $lastname<br>";
+        $q  = $db->prepare("SELECT * FROM schedule WHERE staff_id = ?");
+        $q->bind_param("i", $staff_id);
+        if($q->execute()){
+            $schedule = $q->get_result();
+            while ($visit = $schedule->fetch_assoc()) {
+                $timestamp = strtotime($visit['date']);
+                echo "<button style=\"margin:10px;\">";
+                echo date("j/m/Y H:i", $timestamp);
+                echo "</button>";
+            }
+        }
+        echo "<br>";
     }
 } else {
     echo "błąd podczas wyszukiwania w bazie danych";
